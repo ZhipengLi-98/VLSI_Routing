5ad63ef1e937aa2a1743838968e598e7e258ce0b
李玮祺 <2016011291@secoder.net>
2018-12-18 01:21:55 +0800

    fin

---
+++ maker.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <vector>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include <cmath>
+
+
+#define random(x) (rand()%x)
+
+using namespace std;
+
+int main()
+{
+	int n = 10;
+	int m = 10;
+	int node = 10;
+	int net = 4;
+	int x[1000];
+	int y[1000];
+	int nets[100][100];
+	int s = 2;
+
+	for (int i = 0; i < node; i++)
+	{
+		x[i] = random(n);
+		y[i] = random(m);
+		for (int j = 0; j < i; j++)
+		{
+			if ((x[i] == x[j]) && (y[i] == y[j]))
+			{
+				x[i] = random(n);
+				y[i] = random(m);
+				j = 0;
+				continue;
+			}
+		}
+	}
+	for (int i = 0; i < net; i++)
+	{
+		int num = random(3) + s;
+		nets[i][0] = num;
+		for (int j = 1; j <= num; j++) 
+		{
+			int t = random(node);
+			for (int k = 1; k < j; k++)
+			{
+				if (t == nets[i][k]) 
+				{
+					t = random(node);
+					k = 1;
+					continue; 
+				}
+			}
+			nets[i][j] = t;
+		}
+	}
+
+	cout << n << " " << m << " " << node << " ";
+	cout << net << " ";
+	for (int i = 0; i < node; i++) cout << x[i] << " " << y[i] << " ";
+	for (int i = 0; i < net; i++) 
+	{
+		cout << nets[i][0] << " ";
+		for (int j = 1; j <= nets[i][0]; j++)
+			cout << nets[i][j] << " ";
+	}
+
+	cout << "     " << endl;
+
+	return 0;
+}
--- project/VLSI/VLSI/main.cpp
+++ project/VLSI/VLSI/main.cpp
@@ -2,8 +2,17 @@
 #include <cstring>
 #include <fstream>
 #include <vector>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include <opencv2\opencv.hpp>
+#include <cmath>
 
 #include "node.h"
+
+#define random(x) (rand()%x)
+
+using namespace cv;
 
 class node;
 class point;
@@ -12,6 +21,9 @@ class edge;
 std::string src = "D:\\Virtualbox\\VLSI_Routing\\debug\\";
 std::string fileName = "test.txt";
 std::string workFile = "test1.txt";
+int dxy = 50;
+int br = 5;
+int brl = 1;
 
 int * listX;
 int * listY;
@@ -24,6 +36,12 @@ int nodeNum;
 std::vector<point> nodes;
 std::vector<int> nets;
 std::vector<std::vector<edge>> edges;
+
+using namespace std;
+
+
+int minval;
+std::vector<std::vector<edge>> edgesbest;
 
 bool edgeValid(point a, point b) {
 	for (int i = 0; i < edges.size(); i++) {
@@ -147,7 +165,7 @@ int work(std::string fileName) {
 			P.push_back(T[j]);
 			map[T[j].x * col + T[j].y].s = Target;
 		}
-
+		std::vector<edge> tempEdge;
 		for (int j = 0; j < T.size(); j++) {
 			for (int k = 0; k < map.size(); k++) {
 				map[k].visit = false;
@@ -166,7 +184,7 @@ int work(std::string fileName) {
 			int size = path.size();
 			map[closet.x*col + closet.y].s = Source;
 			bool sourceReach = false;
-			std::vector<edge> tempEdge;
+//			std::vector<edge> tempEdge;
 			while (!sourceReach) {
 				S.push_back(closet);
 				// std::cout << closet.x << " " << closet.y << std::endl;
@@ -194,7 +212,7 @@ int work(std::string fileName) {
 			for (int k = 0; k < tempEdge.size(); k++) {
 				//std::cout << "edge: " << tempEdge[k].start.x << " " << tempEdge[k].start.y << " " << tempEdge[k].end.x << " " << tempEdge[k].end.y << std::endl;
 			}
-			edges.push_back(tempEdge);
+//			edges.push_back(tempEdge);
 
 			for (int k = 0; k < path.size() - 1; k++) {
 				// std::cout << "path: " << path[k].x << " " << path[k].y << std::endl;
@@ -209,6 +227,7 @@ int work(std::string fileName) {
 			std::cout << std::endl;
 			*/
 		}
+		edges.push_back(tempEdge);
 		for (int j = 0; j < nodes.size(); j++) {
 			map[nodes[j].x*col + nodes[j].y].s = Block;
 		}
@@ -218,9 +237,10 @@ int work(std::string fileName) {
 		}
 		//std::cout << "net: " << i << ", length: " << P.size() - 1 << std::endl;
 		rst += P.size() - 1;
-		//std::cout << std::endl;
+		//std::cout << "====" << std::endl;
 	}
 	//system("pause");
+	//std::cout << "===============" << std::endl;
 	file.close();
 	return rst;
 }
@@ -267,15 +287,238 @@ void creatfile(int * reg) {
 	fileout.close();
 }
 
+void drawnode(int x, int y, Mat& picture) {
+	rectangle(picture, cvPoint(dxy * (x + 1) - br, dxy * (y + 1) - br), cvPoint(dxy * (x + 1) + br, dxy * (y + 1) + br), Scalar(255, 0, 0), -1, 1, 0);
+}
+
+void drawline(int x1, int y1, int x2, int y2, const Scalar& color, Mat& picture) {
+	if (x1 == x2) {
+		if (y2 < y1) {
+			int temp = y2;
+			y2 = y1;
+			y1 = temp;
+		}
+		rectangle(picture, cvPoint(dxy * (x1 + 1) - brl, dxy * (y1 + 1) + br + 1), cvPoint(dxy * (x1 + 1) + brl, dxy * (y2 + 1) - br - 1), color, -1, 1, 0);
+	}
+	else {
+		if (x2 < x1) {
+			int temp = x2;
+			x2 = x1;
+			x1 = temp;
+		}
+		rectangle(picture, cvPoint(dxy * (x1 + 1) + br + 1, dxy * (y1 + 1) - brl), cvPoint(dxy * (x2 + 1) - br - 1, dxy * (y2 + 1) + brl), color, -1, 1, 0);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	//std::vector<std::vector<edge>> edges;
+	srand((int)time(0));
 	init();
 	
 	int * reg;
 	reg = new int[netNum];
 
-	//for (int i = 0; i < netNum; i++) std::cin >> reg[i];
+	for (int i = 0; i < netNum; i++) reg[i] = i;
+	for (int i = 0; i < 100 * netNum; i++)
+	{
+		int ix = random(netNum);
+		int iy = random(netNum);
+		if (ix == iy) continue;
+		int temp = reg[ix];
+		reg[ix] = reg[iy];
+		reg[iy] = temp;
+	}
 
+	minval = 2147483647;
+	int lastminval = minval;
+	int counter = 0;
+
+	double runtime;
+	std::cin >> runtime;
+	double dur;
+	clock_t start, end;
+	start = clock();
+	end = clock();
+	dur = (double)(end - start);
+
+	double alpha = 0.95;
+	int R;
+	int temp_acp = 100;
+	double t = 1.0 * (row + col) * netNum * nodeNum;
+	int defmaxf = (row + col) * netNum * nodeNum;
+
+	while ((dur / CLOCKS_PER_SEC) < runtime) {
+
+		std::cout << "tempture = " << t << std::endl;
+
+		//停止条件
+		if (lastminval == minval) {
+			if (counter > 10) {
+				break;
+			}
+			counter++;
+		}
+		else {
+			counter = 0;
+			lastminval = minval;
+		}
+
+		int fi;
+		creatfile(reg);
+		//cout << "beforeget" << endl;
+		fi = work(src + workFile);
+		//cout << "get " << fi << endl;
+		if ((fi > 0) && (fi < minval)) {
+			minval = fi;
+			/*
+			cout << minval << endl;
+			for (int set = 0; set < netNum; set++) {
+				std::cout << "set = " << set << std::endl;
+				for (int k = 0; k < edges[set].size(); k++) {
+					std::cout << "edge: " << edges[set][k].start.x << " " << edges[set][k].start.y << " " << edges[set][k].end.x << " " << edges[set][k].end.y << std::endl;
+				}
+				//std::cout << std::endl;
+			}
+			std::cout << std::endl;
+			*/
+			//cout << "ch1" << endl;
+			edgesbest = edges;
+			//system("pause");
+			//return 0;
+		}
+		//cout << "ch2" << endl;
+		if (fi < 0) fi = defmaxf;
+		//nets.clear();
+		//nodes.clear();
+		//edges.clear();
+		vector<point> eptnodes;
+		vector<int> eptnets;
+		vector<vector<edge>> eptedges;
+		nodes.swap(eptnodes);
+		edges.swap(eptedges);
+		nets.swap(eptnets);
+		//cout << "ch3" << endl;
+
+		R = 0;
+		while (true)
+		{
+			//平衡条件
+			if (R > temp_acp) {
+				break;
+			}
+
+			//选择解
+			int ix = random(netNum);
+			int iy = random(netNum);
+			//cout << netNum << endl;
+			//cout << ix << "," << iy << endl;
+			while (ix == iy)
+			{
+				ix = random(netNum);
+				//cout << ix << "," << iy << endl;
+			}
+			int * regj = new int[netNum];
+			for (int i = 0; i < netNum; i++) regj[i] = reg[i];
+			int temp = regj[ix];
+			regj[ix] = regj[iy];
+			regj[iy] = temp;
+
+			//cout << "ch4" << endl;
+
+			//计算f
+			int fj;
+			creatfile(regj);
+			//cout << "beforeget" << endl;
+			fj = work(src + workFile);
+			//cout << "get " << fi << endl;
+			if ((fj > 0) && (fj < minval)) {
+				minval = fj;
+				/*
+				cout << minval << endl;
+				for (int set = 0; set < netNum; set++) {
+					std::cout << "set = " << set << std::endl;
+					for (int k = 0; k < edges[set].size(); k++) {
+						std::cout << "edge: " << edges[set][k].start.x << " " << edges[set][k].start.y << " " << edges[set][k].end.x << " " << edges[set][k].end.y << std::endl;
+					}
+					//std::cout << std::endl;
+				}
+				std::cout << std::endl;
+				*/
+				edgesbest = edges;
+				//system("pause");
+				//return 0;
+			}
+			if (fj < 0) fj = defmaxf;
+			//nets.clear();
+			//nodes.clear();
+			//edges.clear();
+			vector<point> eptnodes;
+			vector<int> eptnets;
+			vector<vector<edge>> eptedges;
+			nodes.swap(eptnodes);
+			edges.swap(eptedges);
+			nets.swap(eptnets);
+
+			//接受判断
+			if (fj < fi) {
+				fi = fj;
+				temp = reg[ix];
+				reg[ix] = reg[iy];
+				reg[iy] = temp;
+				R++;
+			}
+			else {
+				double p = exp((-1.0 * (fj - fi)) / t);
+				double ran = (1.0 * random(100001)) / 100000;
+				if (ran < p) {
+					fi = fj;
+					temp = reg[ix];
+					reg[ix] = reg[iy];
+					reg[iy] = temp;
+					R++;
+				}
+			}
+
+			R++;
+		};
+
+		//温度下降
+		t = t * alpha;
+
+		end = clock();
+		dur = (double)(end - start);
+	}
+
+	//std::cout << minval << std::endl;
+	if (minval == 2147483647) {
+		system("pause");
+		return 0;
+	}
+
+	Mat picture((row + 2) * dxy, (col + 2) * dxy, CV_8UC3, Scalar(254, 254, 254));
+	for (int i = 0; i < nodeNum; i++)
+	{
+		drawnode(listX[i], listY[i], picture);
+	}
+	//drawline(1, 1, 2, 1, picture);
+	for (int set = 0; set < netNum; set++) {
+		int r = random(255);
+		int g = random(255);
+		int b = random(255);
+		std::cout << "set = " << set << std::endl;
+		for (int k = 0; k < edgesbest[set].size(); k++) {
+			std::cout << "edge: " << edgesbest[set][k].start.x << " " << edgesbest[set][k].start.y << " " << edgesbest[set][k].end.x << " " << edgesbest[set][k].end.y << std::endl;
+			drawline(edgesbest[set][k].start.x, edgesbest[set][k].start.y, edgesbest[set][k].end.x, edgesbest[set][k].end.y, Scalar(b, g, r), picture);
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << minval << std::endl;
+
+	imshow("line", picture);
+	waitKey(-1);
+
+	/*
 	for (int i = 0; i < netNum; i++) {
 		for (int j = 0; j < netNum; j++) {
 			if (i == j) continue;
@@ -291,17 +534,16 @@ int main(int argc, char *argv[]) {
 				int rst = work(src + workFile);
 				std::cout << rst << std::endl;
 
+				if ((rst > 0) && (rst < minval)) {
+					minval = rst;
+					edgesbest = edges;
+				}
 				nets.clear();
 				nodes.clear();
 				edges.clear();
 			}
 		}
 	}
-	/*
-	creatfile(reg);
-
-	int rst = work(src + workFile);
-	std::cout << rst << std::endl;
 	*/
 
 	system("pause");
